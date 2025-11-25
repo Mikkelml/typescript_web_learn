f0a411d017081bf66ff3644f3cefcc231b0b83cc
Mikkelml <169037561+Mikkelml@users.noreply.github.com>
2025-11-25 20:39:16 +0100

    correction of different elements including sc

--- my-portfolio/src/components/Footer.tsx
+++ my-portfolio/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-900 text-white py-8">
+    <footer className="bg-black text-white py-8">
       <div className="container mx-auto px-6 text-center">
         <p className="mb-4">
           &copy; {new Date().getFullYear()} Victor Ferrold / Mikkel Pedersen - All rights reserved.
